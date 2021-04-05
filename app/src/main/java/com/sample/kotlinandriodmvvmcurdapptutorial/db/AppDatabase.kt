4b1909a2dbd1fbbe8a1806a09c44edb0519d02a9
saurabh <saurabh.mishra@nurtr.com>
2021-04-05 11:19:48 +0530

    Initial Commit

--- app/src/main/java/com/sample/kotlinandriodmvvmcurdapptutorial/db/AppDatabase.kt
+++ app/src/main/java/com/sample/kotlinandriodmvvmcurdapptutorial/db/AppDatabase.kt
@@ -6,23 +6,25 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Subscriber::class],version = 1)
+@Database(entities = [Subscriber::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun subscriberDAO() : SubscriberDAO
+    abstract fun subscriberDAO(): SubscriberDAO
 
-    companion object{
+    companion object {
         @Volatile
-        private var INSTANCE : AppDatabase? = null
-        fun getInstance(context: Context):AppDatabase{
-            synchronized(this){
+        private var INSTANCE: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
                 var instance = INSTANCE
-                if(instance==null){
+                if (instance == null) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         AppDatabase::class.java,
                         "subscriber_data_database"
-                    ).build()
+                    )
+                       // .fallbackToDestructiveMigration() // this use use when you want add new version(remove all added new db) of data base, but make sure  tag added in manifest file android:allowBackup="false"
+                        .build()
                 }
                 return instance
             }
